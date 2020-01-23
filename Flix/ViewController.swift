08164dc4abdd74533b0d6612a7d98ce4c0f035c1
Pallav Agarwal <agpallav@gmail.com>
2020-01-22 19:06:29 -0500

    Initial

---
+++ Flix/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flix
+//
+//  Created by Pallav Agarwal on 1/22/20.
+//  Copyright © 2020 Pallav Agarwal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
